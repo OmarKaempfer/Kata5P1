e9411ef9f1f2f03fde7fa1928796b3dd489dffb0
OmarKaempfer <omarkaempfer@gmail.com>
2018-11-27 18:21:25 +0000

    Kata5P1 v2 : Create a new table within the program

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -4,7 +4,9 @@ import java.sql.*;
 
 public class Kata5P1 {
     public static void main(String[] args) {
-        selectAll();
+        Connection connection = connect();
+        selectAll(connection);
+        createNewTable(connection);
     }
 
     private static Connection connect() {
@@ -19,11 +21,10 @@ public class Kata5P1 {
         return connection;
     }
 
-    private static void selectAll() {
+    private static void selectAll(Connection connection) {
         String sqlQuery = "SELECT * FROM PEOPLE";
 
-        try(Connection connection = connect();
-            Statement statement = connection.createStatement();
+        try(Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sqlQuery)) {
             while(resultSet.next()) {
                 System.out.println(resultSet.getInt("id") + "\t" +
@@ -32,6 +33,21 @@ public class Kata5P1 {
                         resultSet.getString("Departamento") + "\t");
             }
         } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void createNewTable(Connection connection) {
+        String url = "jdbc:sqlite:kata5.db";
+
+        String sql = "CREATE TABLE IF NOT EXISTS EMAIL (\n"
+                + " id integer PRIMARY KEY AUTOINCREMENT,\n"
+                + " Mail text NOT NULL);";
+
+        try(Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+            System.out.println("Table created");
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
